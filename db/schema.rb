69ba0b79f19d24cb6e21f9f321154c23ffc2f606
Mitch Pateman <mitch_pateman@hotmail.com>
2016-12-21 12:01:04 -0500

    Updating more seeds and db

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20161220180318) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "records", force: :cascade do |t|
     t.string   "species"
     t.float    "weight"
